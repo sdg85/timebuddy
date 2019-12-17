aa69b7e75ec65b8058a32017156dc0187de451d8
Simkha -Home <simkha.danial-gorgis@orango.se>
2019-12-17 21:27:07 +0100

    add routes model.

---
+++ lib/routes/routes.dart
@@ -0,0 +1,17 @@
+import 'package:timebuddy/screens/colleges_screen.dart';
+import 'package:timebuddy/screens/login_screen.dart';
+import 'package:timebuddy/screens/mail_screen.dart';
+import 'package:timebuddy/screens/mypage_screen.dart';
+import 'package:timebuddy/screens/schedule_screen.dart';
+import 'package:timebuddy/screens/stamp_screen.dart';
+
+class Routes {
+
+  static const String schedule = ScheduleScreen.routeName;
+  static const String login = LoginScreen.routeName;
+  static const String stamp = StampScreen.routeName;
+  static const String mail = MailScreen.routeName;
+  static const String colleges = CollegesScreen.routeName;
+  static const String mypage = MyPageScreen.routeName;
+  
+}
