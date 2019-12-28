ac7a5baa1b4e25d33a2b8370d98d5a7324830c92
Simkha -Home <simkha.danial-gorgis@orango.se>
2019-12-28 21:34:56 +0100

    create schedule screen.

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:timebuddy/widgets/calendar.dart';
 import 'package:timebuddy/widgets/layout.dart';
+import 'package:timebuddy/widgets/scheduled_employees_list.dart';
 
 class ScheduleScreen extends StatelessWidget {
-  static const routeName = "/schedule";
+  static const String routeName = "/schedule";
   const ScheduleScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Layout(
       title: "Schema",
-      child: Center(
-        child: Text("Schema"),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Calendar(),
+          ),
+          ScheduledEmployeesList()
+        ],
       ),
     );
   }
