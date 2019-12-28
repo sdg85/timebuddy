57fb7c65302de5293688dd89462bcf76f8d39a92
Simkha -Home <simkha.danial-gorgis@orango.se>
2019-12-28 21:30:27 +0100

    add work shift provider.

---
+++ lib/providers/work_shift_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:timebuddy/models/employee_shift.dart';
+import 'package:timebuddy/models/shift_date.dart';
+
+class WorkShiftProvider with ChangeNotifier {
+  DateTime _selectedDay = DateTime.now();
+
+  DateTime get selectedDay => _selectedDay;
+
+  set selectedDay(DateTime selectedDay) {
+    _selectedDay = selectedDay;
+    notifyListeners();
+  }
+
+  ShiftDate getEmployeeDayShift(EmployeeShift employeeShift){
+    final date = DateTime(_selectedDay.year,
+            _selectedDay.month, _selectedDay.day);
+    
+    return employeeShift.shiftDates.firstWhere((shift) {
+          final shiftDate = shift.startDate;
+          return DateTime(shiftDate.year, shiftDate.month, shiftDate.day) ==
+              DateTime(date.year, date.month, date.day);
+        }, orElse: () => null);
+  }
+}
