84c6e5bf650ff443d447d730955bb476665ebef4
Simkha -Home <simkha.danial-gorgis@orango.se>
2020-01-05 21:23:20 +0100

    add calendar toggle functionality.

--- lib/providers/work_shift_provider.dart
+++ lib/providers/work_shift_provider.dart
@@ -4,6 +4,7 @@ import 'package:timebuddy/models/shift_date.dart';
 
 class WorkShiftProvider with ChangeNotifier {
   DateTime _selectedDay = DateTime.now();
+  bool _toggleCalendar = true;
 
   DateTime get selectedDay => _selectedDay;
 
@@ -12,14 +13,21 @@ class WorkShiftProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  ShiftDate getEmployeeDayShift(EmployeeShift employeeShift){
-    final date = DateTime(_selectedDay.year,
-            _selectedDay.month, _selectedDay.day);
-    
+  bool get toggleCalendar => _toggleCalendar;
+
+  set toggleCalendar(bool toggleCalendar) {
+    _toggleCalendar = toggleCalendar;
+    notifyListeners();
+  }
+
+  ShiftDate getEmployeeDayShift(EmployeeShift employeeShift) {
+    final date =
+        DateTime(_selectedDay.year, _selectedDay.month, _selectedDay.day);
+
     return employeeShift.shiftDates.firstWhere((shift) {
-          final shiftDate = shift.startDate;
-          return DateTime(shiftDate.year, shiftDate.month, shiftDate.day) ==
-              DateTime(date.year, date.month, date.day);
-        }, orElse: () => null);
+      final shiftDate = shift.startDate;
+      return DateTime(shiftDate.year, shiftDate.month, shiftDate.day) ==
+          DateTime(date.year, date.month, date.day);
+    }, orElse: () => null);
   }
 }
