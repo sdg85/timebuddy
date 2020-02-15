e127262e75d2fb5b236b566681b9eddb2fa93438
Simkha -Home <simkha.danial-gorgis@orango.se>
2020-02-15 22:37:55 +0100

    service for getting work shift data from db.

---
+++ lib/services/work_shift_service.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+class WorkShiftService {
+  final db = Firestore.instance;
+
+  //get all work shifts for the given month
+  Future<QuerySnapshot> getAllWorkShiftsByMonth(DateTime date) async {
+    final period = DateFormat("yyyyMM").format(date);
+    return await db.collection("work_shift")
+    .where("period", isEqualTo: period)
+    .getDocuments();
+  }
+
+  //get logged in user workshifts by month
+  Future<QuerySnapshot> getUserWorkShiftsByMonth(String userId, DateTime date) async {
+    final period = DateFormat("yyyyMM").format(date);
+     
+     return await db.collection("work_shift")
+    .where("employeeId", isEqualTo: userId)
+    .where("period", isEqualTo: period)
+    .getDocuments();
+  }
+
+  Future<QuerySnapshot> getWorkShiftsByDay(DateTime date) async {
+     return await db.collection("work_shift")
+    .where("date", isEqualTo: DateFormat("yyyyMMdd").format(date))
+    .getDocuments();
+  }
+
+  void getUsersByIds(){
+    db.collection("employees").where("uid", arrayContains: ['0a7UY5KXDV5V3Gmmtiip', '6yEitexpEhuZd3ac3TBW']).getDocuments()
+    .then((doc) => print(doc.documents))
+    .catchError((error) => print(error));
+  }
+}
