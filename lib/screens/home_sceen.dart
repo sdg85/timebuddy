86b6735266a8f663ad9c4599b4681682a4e11917
Simkha -Home <simkha.danial-gorgis@orango.se>
2020-02-15 22:54:41 +0100

    change ui for home screen.

--- lib/screens/home_sceen.dart
+++ lib/screens/home_sceen.dart
@@ -1,37 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:timebuddy/widgets/layout.dart';
-import "../services/auth_service.dart";
 
 class HomeScreen extends StatelessWidget {
-  // final AuthService auth = AuthService();
+  static const String routeName = "/home";
 
   HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<AuthService>(context);
-    return Layout(
-      title: "Home",
-      child: Container(
-        child: Center(
-          child: Column(
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: InkWell(
+          child: Row(
             children: <Widget>[
-              Text(
-                "WELCOME TO HOME SCREEN!",
-                style: TextStyle(
-                  color: Colors.blue[300],
-                ),
-              ),
-              FlatButton(child: Text("logout"),
-              onPressed: () async {
-                await auth.signOut();
-              },
-              )
+              Text("Januari"),
+              Icon(Icons.arrow_drop_down),
             ],
           ),
+          onTap: (){},
         ),
+        titleSpacing: 0.0,
       ),
+      body: Container(),
     );
-  }
+}
 }
