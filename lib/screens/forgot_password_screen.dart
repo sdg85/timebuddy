052861e3068ccb02fc9f3cb1eed98605572325b6
Simkha -Home <simkha.danial-gorgis@orango.se>
2020-03-18 23:20:25 +0100

    place the text after the email textfield.

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -32,90 +32,104 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         backgroundColor: Color(0xffF2F4F9),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: _loading ? InProgressLoader() : Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "Återställ ditt lösenord",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16.0
-                  ),
-                ),
-                SizedBox(height: 5.0,),
-                Text(
-                  "Ange den e-postadress som du använde när du registrerade dig."
-                  "Vi skickar ett e-postmeddelande med en länk där du kan återställa ditt lösenord.",
-                  style: TextStyle(
-                    fontSize: 12.0,
-                    color: Colors.grey,
+          child: _loading
+              ? InProgressLoader()
+              : Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Återställ ditt lösenord",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 16.0),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // Divider(
+                      //   color: Colors.grey,
+                      // ),
 
+                      TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyle(height: 1.0),
+                        decoration: InputDecoration(
+                          hintText: "E-postadress",
+                          hintStyle: TextStyle(fontWeight: FontWeight.w500),
+                          fillColor: Colors.white,
+                          filled: true,
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (!EmailValidator.validate(value)) {
+                            return "Var god och ange ett giltigt e-postadress";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) => _email = value,
+                        initialValue: _email,
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Ange den e-postadress som du använde när du registrerade dig."
+                        "Vi skickar ett e-postmeddelande med en länk där du kan återställa ditt lösenord.",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width,
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          child: Text("Skicka verfikations mail"),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              _formKey.currentState.save();
+                              setState(() => _loading = true);
+                              _formKey.currentState.save();
+                              final result = await auth.forgotPassword(_email);
+
+                              if (result)
+                                Navigator.pop(context);
+                              else
+                                setState(() {
+                                  _errorMessage =
+                                      "Kunde inte hitta användare med detta e-postadress";
+                                  _loading = false;
+                                });
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      _errorMessage != null
+                          ? Text(
+                              _errorMessage,
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w500),
+                            )
+                          : Text(""),
+                    ],
                   ),
                 ),
-                SizedBox(height: 10,),
-                Divider(
-                  color: Colors.grey,
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(height: 1.0),
-                  decoration: InputDecoration(
-                    hintText: "E-postadress",
-                    hintStyle: TextStyle(fontWeight: FontWeight.w500),
-                    fillColor: Colors.white,
-                    filled: true,
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (!EmailValidator.validate(value)) {
-                      return "Var god och ange ett giltigt e-postadress";
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _email = value,
-                  initialValue: _email,
-                ),
-                Divider(height: 10,),
-                ButtonTheme(
-                  minWidth: MediaQuery.of(context).size.width,
-                  child: RaisedButton(
-                    textColor: Colors.white,
-                    child: Text("Skicka verfikations mail"),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        _formKey.currentState.save();
-                        setState(() => _loading = true);
-                        _formKey.currentState.save();
-                        final result = await auth.forgotPassword(_email);
-                        
-                        if (result) 
-                          Navigator.pop(context);
-                        else                          
-                          setState(() {
-                            _errorMessage =
-                                "Kunde inte hitta användare med detta e-postadress";
-                                _loading = false;
-                          });
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(height: 40,),
-                _errorMessage != null ? Text(_errorMessage, style: TextStyle(
-                  color: Colors.red, 
-                  fontSize: 13, 
-                  fontWeight: FontWeight.w500),
-                  ) : Text(""),
-              ],
-            ),
-          ),
         ),
       ),
     );
