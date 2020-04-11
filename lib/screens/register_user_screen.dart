2ac5986359b442eb56c2b458f33ac1245c3418e7
hp 650 <exceptional85@hotmail.com>
2020-04-11 19:15:48 +0200

    code refactor

--- lib/screens/register_user_screen.dart
+++ lib/screens/register_user_screen.dart
@@ -1,6 +1,5 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
-import 'package:timebuddy/widgets/layout.dart';
 
 import '../models/user.dart';
 import '../services/auth_service.dart';
@@ -37,7 +36,7 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
                   ? Text("User successfully created")
                   : Text(
                       "Något gick fel vid inloggningen var vänlig försök igen"),
-                      Spacer(),
+              Spacer(),
               valid ? Icon(Icons.check_circle) : Icon(Icons.error_outline)
             ],
           ),
@@ -46,112 +45,106 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
 
   @override
   Widget build(BuildContext buildContext) {
-    return Layout(
-      title: "Admin",
-      appBarTitleWidget: Text("Registrera"),
-      child: Builder(
-        builder: (ctx) => loading
-            ? InProgressLoader()
-            : Padding(
-                padding: EdgeInsets.all(30.0),
+    return Builder(
+      builder: (ctx) => loading
+          ? InProgressLoader()
+          : Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Container(
                 child: Container(
-                  child: Container(
-                    height: double.maxFinite,
-                    child: SingleChildScrollView(
-                      child: Form(
-                        key: _key,
-                        child: Column(
-                          children: <Widget>[
-                            TextFormField(
-                              onSaved: (val) => firstname = val,
-                              decoration: InputDecoration(labelText: "Förnamn"),
-                              validator: (val) {
-                                return val == ""
-                                    ? "Förnamn är obligatoriskt."
-                                    : null;
-                              },
-                            ),
-                            TextFormField(
-                              onSaved: (val) => lastname = val,
-                              decoration:
-                                  InputDecoration(labelText: "Efternamn"),
-                              validator: (val) {
-                                return val == ""
-                                    ? "Efternamn är obligatoriskt."
-                                    : null;
-                              },
-                            ),
-                            TextFormField(
-                              keyboardType: TextInputType.emailAddress,
-                              onSaved: (val) => email = val,
-                              decoration: InputDecoration(labelText: "E-post"),
-                              validator: (value) {
-                                if (!EmailValidator.validate(value)) {
-                                  return "Var god och ange ett giltigt e-postadress";
-                                }
-                                return null;
-                              },
-                            ),
-                            TextFormField(
-                              validator: (value) {
-                                if (value == null || value == "") {
-                                  return "Ange ett lösenord";
-                                }
-                                if (value.length < 6) {
-                                  return "Lösenorder måste innehålla minst 6 tecken";
-                                }
+                  height: double.maxFinite,
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: _key,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            onSaved: (val) => firstname = val,
+                            decoration: InputDecoration(labelText: "Förnamn"),
+                            validator: (val) {
+                              return val == ""
+                                  ? "Förnamn är obligatoriskt."
+                                  : null;
+                            },
+                          ),
+                          TextFormField(
+                            onSaved: (val) => lastname = val,
+                            decoration: InputDecoration(labelText: "Efternamn"),
+                            validator: (val) {
+                              return val == ""
+                                  ? "Efternamn är obligatoriskt."
+                                  : null;
+                            },
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            onSaved: (val) => email = val,
+                            decoration: InputDecoration(labelText: "E-post"),
+                            validator: (value) {
+                              if (!EmailValidator.validate(value)) {
+                                return "Var god och ange ett giltigt e-postadress";
+                              }
+                              return null;
+                            },
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value == "") {
+                                return "Ange ett lösenord";
+                              }
+                              if (value.length < 6) {
+                                return "Lösenorder måste innehålla minst 6 tecken";
+                              }
 
-                                return null;
-                              },
-                              onSaved: (val) => password = val,
-                              decoration:
-                                  InputDecoration(labelText: "Lösenord"),
-                            ),
-                            SizedBox(
-                              height: 20.0,
-                            ),
-                            SizedBox(
-                              width: double.maxFinite - 30.0,
-                              child: RaisedButton(
-                                child: Text(
-                                  "Registrera",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                                onPressed: () async {
-                                  if (_key.currentState.validate()) {
-                                    _key.currentState.save();
-                                    setState(() => loading = true);
-                                    var user = User(
-                                        email: email,
-                                        firstname: firstname,
-                                        lastname: lastname,
-                                        photo: photo);
-                                    final result =
-                                        await auth.newUser(password, user);
-
-                                    loading = false;
-                                    final snackBar =
-                                        createSnackBar(valid: result);
-                                    Scaffold.of(ctx).showSnackBar(snackBar);
-                                  }
-                                  if (!mounted) return;
-                                  setState(() {
-                                    autoValidate = true;
-                                  });
-                                },
-                                color: Color(0xff017ACD),
+                              return null;
+                            },
+                            onSaved: (val) => password = val,
+                            decoration: InputDecoration(labelText: "Lösenord"),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          SizedBox(
+                            width: double.maxFinite - 30.0,
+                            child: RaisedButton(
+                              child: Text(
+                                "Registrera",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
                               ),
+                              onPressed: () async {
+                                if (_key.currentState.validate()) {
+                                  _key.currentState.save();
+                                  setState(() => loading = true);
+                                  var user = User(
+                                      email: email,
+                                      firstname: firstname,
+                                      lastname: lastname,
+                                      photo: photo);
+                                  final result =
+                                      await auth.newUser(password, user);
+
+                                  loading = false;
+                                  final snackBar =
+                                      createSnackBar(valid: result);
+                                  Scaffold.of(ctx).showSnackBar(snackBar);
+                                }
+                                if (!mounted) return;
+                                setState(() {
+                                  autoValidate = true;
+                                });
+                              },
+                              color: Color(0xff017ACD),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
               ),
-      ),
+            ),
     );
   }
 }
