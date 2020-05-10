be582eede31ce0901ee48a103a4ab5a186c77ca5
hp 650 <exceptional85@hotmail.com>
2020-05-10 20:35:51 +0200

    implement basic create new work shift.

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:timebuddy/models/work_shift.dart';
 
 import '../models/user.dart';
 
 class FirestoreService {
   final _firestoreEmployeeDb = Firestore.instance.collection("employees");
+  final _firestoreWorkShiftDb = Firestore.instance.collection("work_shift");
 
   Future createUser(String uid, User user) async {
     try {
+      //set default profile image if null
       await _firestoreEmployeeDb.document(uid).setData(user.toJson());
 
     } catch (e) {
@@ -19,7 +22,7 @@ class FirestoreService {
         try{
           final userData = await _firestoreEmployeeDb.document(uid).get();
           print(userData.data);
-          return User.fromData(userData.data);
+          return User.fromData(userData.documentID, userData.data);
         }
         catch(e){
           print(e.toString());
@@ -28,9 +31,23 @@ class FirestoreService {
       }
 
       Future<List<User>> getAllEmployees() async {
+        print("fetching employees from database.");
         return (await _firestoreEmployeeDb.getDocuments())
         .documents
-        .map( (ds) => User.fromData(ds.data))
+        .map( (ds) => User.fromData(ds.documentID, ds.data))
         .toList();
+      }
+
+      Future createWorkShift(WorkShift workShift) async {
+        try{
+          await _firestoreWorkShiftDb.document()
+          .setData(workShift.toJson());
+
+          return true;
+        }
+        catch(e){
+          print(e.message);
+          return false;
+        }
       }
 }
